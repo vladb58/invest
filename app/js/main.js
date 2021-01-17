2188cc6a861bbd93c751140b0cf89908397237cd
vladb58 <belonogoff.vladislaw@yandex.ru>
2021-01-17 16:27:19 +0300

    finish desctop

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,7 @@
 $(function () {
+  $('.answers-questions__item-title').on('click', function () {
+    $(this).siblings().slideToggle();
+    $(this).toggleClass('answers-questions__item-title--active');
+  });
  
 })
