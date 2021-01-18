954cf2ec44375b191d3e83011053f8008b035e2a
vladb58 <belonogoff.vladislaw@yandex.ru>
2021-01-18 18:13:01 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -3,5 +3,10 @@ $(function () {
     $(this).siblings().slideToggle();
     $(this).toggleClass('answers-questions__item-title--active');
   });
+
+  $('.stages__link').on('click', function () {
+    $(this).nextAll().slideToggle();
+    $(this).toggleClass('stages__link--active');
+  });
  
 })
